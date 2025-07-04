6c347b63575ae4ea8c6bd1f66387ccae3b9e6aba
sigminit <148963144+sigminit@users.noreply.github.com>
2025-07-04 01:35:43 -0700

    Create ss.js

---
+++ ss.js
@@ -0,0 +1,21 @@
+var email = $n.AccountStore.getCurrentAccount().emailAddress;
+const openButton = document.getElementById("settings-icon-btn");
+openButton.click();
+const observer = new MutationObserver(mutations=>{
+    mutations.forEach(mutation=>{
+        mutation.addedNodes.forEach(node=>{
+            if (node.nodeType === 1 && node.classList?.contains('panel-container')) {
+                const interval = setInterval(()=>{
+                    const adminPanelItem = node.querySelector('[data-testid="admin_control_panel"]');
+                    if (adminPanelItem) {
+                        clearInterval(interval);
+                        openButton.click();
+                        var str = adminPanelItem.textContent.trim()
+                        console.log(email+" has "+str)
+                    }
+                },100);
+                node.style.display = 'none';
+                observer.disconnect();
+            }
+        });});});
+observer.observe(document.body, {childList: true,subtree: true});
